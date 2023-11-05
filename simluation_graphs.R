f47bed089384643a4364c8faaf914bf948c310d2
jwestreich <jwestreich1@gmail.com>
2023-11-05 00:18:21 -0400

    actually saved results graphs

--- simluation_graphs.R
+++ simluation_graphs.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+folder_path<-"C:/Users/jwest/github/safe_withdrawl_rate/simulation_graphs/"
 
 w3_t46_i0<-read.csv("C:/Users/jwest/github/safe_withdrawl_rate/simluation_results/withdraw3_training1946_inflation0_10000trials.csv")%>%
   mutate(withdrawl=3,
@@ -40,7 +41,7 @@ training_data<-data%>%
   filter(withdrawl==4 & inflation==0)%>%
   mutate(training=as.factor(training))%>%
   rename(`Start of\nTraining Period`=training)
-ggplot(training_data, aes(x = retirement_length, y = success_prob, color = `Start of\nTraining Period`)) +
+training<-ggplot(training_data, aes(x = retirement_length, y = success_prob, color = `Start of\nTraining Period`)) +
   geom_line(size=1.2) +
   geom_vline(aes(xintercept = 30), color = "black", size = .5) +
   labs(x = "Length of Retirement", y = "Confidence of Success")+
@@ -54,13 +55,13 @@ ggplot(training_data, aes(x = retirement_length, y = success_prob, color = `Star
         panel.grid.major = element_line(color = "gray"),
         panel.grid.minor = element_line(color = "gray"),
         legend.title = element_text(hjust = 0.5))
-#ggsave(paste0(folder_path,"example_30yr.png"), plot = example_30yr)
+ggsave(paste0(folder_path,"training.png"), plot = training)
 
 withdrawl_data<-data%>%
   filter(training==1946 & inflation==0)%>%
   mutate(withdrawl=paste0(withdrawl,"%"))%>%
   rename(`Withdrawl Rate`=withdrawl)
-ggplot(withdrawl_data, aes(x = retirement_length, y = success_prob, color = `Withdrawl Rate`)) +
+withdrawl<-ggplot(withdrawl_data, aes(x = retirement_length, y = success_prob, color = `Withdrawl Rate`)) +
   geom_line(size=1.2) +
   geom_vline(aes(xintercept = 30), color = "black", size = .5) +
   labs(x = "Length of Retirement", y = "Confidence of Success")+
@@ -74,12 +75,13 @@ ggplot(withdrawl_data, aes(x = retirement_length, y = success_prob, color = `Wit
         panel.grid.major = element_line(color = "gray"),
         panel.grid.minor = element_line(color = "gray"),
         legend.title = element_text(hjust = 0.5))
+ggsave(paste0(folder_path,"withdrawl.png"), plot = withdrawl)
 
 inflation_w3_data<-data%>%
   filter(training==1946 & withdrawl==3)%>%
   mutate(inflation=paste0(inflation,"%"))%>%
   rename(`Long-Run\nInflation Rate`=inflation)
-ggplot(inflation_w3_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
+inflation3<-ggplot(inflation_w3_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
   geom_line(color="#00BA38", size = 1.2) +
   geom_vline(aes(xintercept = 30), color = "black", size = .5) +
   labs(x = "Length of Retirement", y = "Confidence of Success") +
@@ -94,12 +96,13 @@ ggplot(inflation_w3_data, aes(x = retirement_length, y = success_prob, linetype 
         panel.grid.minor = element_line(color = "gray"),
         legend.title = element_text(hjust = 0.5),
         legend.key.width = unit(1.5, "cm"))
+ggsave(paste0(folder_path,"inflation3.png"), plot = inflation3)
 
 inflation_w4_data<-data%>%
   filter(training==1946 & withdrawl==4)%>%
   mutate(inflation=paste0(inflation,"%"))%>%
   rename(`Long-Run\nInflation Rate`=inflation)
-ggplot(inflation_w4_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
+inflation4<-ggplot(inflation_w4_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
   geom_line(color="blue", size = 1.2) +
   geom_vline(aes(xintercept = 30), color = "black", size = .5) +
   labs(x = "Length of Retirement", y = "Confidence of Success") +
@@ -114,12 +117,13 @@ ggplot(inflation_w4_data, aes(x = retirement_length, y = success_prob, linetype 
         panel.grid.minor = element_line(color = "gray"),
         legend.title = element_text(hjust = 0.5),
         legend.key.width = unit(1.5, "cm"))
+ggsave(paste0(folder_path,"inflation4.png"), plot = inflation4)
 
 inflation_w5_data<-data%>%
   filter(training==1946 & withdrawl==5)%>%
   mutate(inflation=paste0(inflation,"%"))%>%
   rename(`Long-Run\nInflation Rate`=inflation)
-ggplot(inflation_w5_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
+inflation5<-ggplot(inflation_w5_data, aes(x = retirement_length, y = success_prob, linetype = `Long-Run\nInflation Rate`)) +
   geom_line(color="red", size = 1.2) +
   geom_vline(aes(xintercept = 30), color = "black", size = .5) +
   labs(x = "Length of Retirement", y = "Confidence of Success") +
@@ -134,3 +138,4 @@ ggplot(inflation_w5_data, aes(x = retirement_length, y = success_prob, linetype 
         panel.grid.minor = element_line(color = "gray"),
         legend.title = element_text(hjust = 0.5),
         legend.key.width = unit(1.5, "cm"))
+ggsave(paste0(folder_path,"inflation5.png"), plot = inflation5)
